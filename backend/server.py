5bfe951251ccf97252fbf20544fcefec2b59332c
smmahamudul <mahamudul15-13707@diu.edu.bd>
2026-06-13 11:49:42 +0900

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -266,4 +266,11 @@ def image_files(filename):
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Grocery Store Management System")
-    app.run(port=5000, debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
